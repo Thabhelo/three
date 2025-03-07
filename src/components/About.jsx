b7b6223b5553f5ca48a82d2ca33619507682e298
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2025-03-07 00:54:37 -0600

    added line breaks inbetween overview paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,9 +50,9 @@ const About = () => {
         I have a strong foundation in Computer Science and Mathematics, I'm a 8x hackathon winner, and I'm a Software Engineering Intern at Amazon.
         I'm a growing Full Stack Software Engineer, with a keen interest in web and VR/AR development. I'm also very passionate in Data Science, Machine Learning and Cloud Engineering.
         I spend most time working with Java, Python, JavaScript, TypeScript and Swift.
+        <br /><br />
         I'm a quick learner and collaborate closely with teams to create efficient, scalable, and user-friendly solutions that solve real-world problems. 
         I'm also a huge fan of open-source and I'm always looking for ways to give back to the community - whether it’s fixing bugs, writing documentation, or just adding a README that doesn’t leave people questioning their career choices :-)
-
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
