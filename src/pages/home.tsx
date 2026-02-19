cc37cbbfdecad0a6221759e7d114db590de4e0bf
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2026-02-19 03:41:40 -0600

    minor text updates

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -45,7 +45,7 @@ const PROJECTS = [
 
 const EXPERIENCES = [
   {
-    title: "Founding Engineer",
+    title: "Software Engineer",
     company: "DeepUbuntu Labs",
     location: "Remote",
     date: "Jan 2025 - Present",
@@ -215,10 +215,10 @@ export default function Home() {
 
                 <div className="space-y-6 text-lg text-muted-foreground leading-relaxed">
                   <p>
-                    I have a strong foundation in Computer Science and Mathematics, I'm an 8x hackathon winner, and I'm a Software Engineering Intern at Amazon. I'm a growing Full Stack Software Engineer, with a keen interest in web and VR/AR development. I'm also very passionate in Data Science, Machine Learning and Cloud Engineering. I spend most time working with Java, Python, JavaScript, TypeScript and Swift.
+                    CS, Math and Physics, 11x hackathon titles, Formerly Software Engineering Intern at Amazon.
                   </p>
                   <p>
-                    I'm a quick learner and collaborate closely with teams to create efficient, scalable, and user-friendly solutions that solve real-world problems. I'm also a huge fan of open-source and I'm always looking for ways to give back to the community - whether it’s fixing bugs, writing documentation, or just adding a README that doesn’t leave people questioning their career choices :-)
+                    I'm a huge fan of open-source too
                   </p>
                 </div>
 
